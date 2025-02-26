99e1e8cab89f12e50fee7fcc6181596f6f763c49
André Luís Conceição Azevedo <130715051+Andrezito10@users.noreply.github.com>
2025-02-26 23:56:27 +0000

    quarto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentInput = '';
 let operator = '';
 let previousInput = '';
+let history = []; 
 
 function appendNumber(number) {
     currentInput += number;
@@ -22,8 +23,14 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+function backspace() {
+    currentInput = currentInput.slice(0, -1); 
+    document.getElementById('display').value = currentInput; 
+}
+
 function calculateResult() {
     if (currentInput === '' || previousInput === '') return;
+
     let result;
     switch (operator) {
         case '+':
@@ -41,6 +48,27 @@ function calculateResult() {
         default:
             return;
     }
+
+   
+    let operation = `${previousInput} ${operator} ${currentInput} = ${result}`;
+    history.push(operation);
+
+    
+    updateHistory();
+
+   
     currentInput = result.toString();
     document.getElementById('display').value = currentInput;
+}
+
+function updateHistory() {
+    let historyDiv = document.getElementById('history');
+    historyDiv.innerHTML = ''; // Limpa o histórico atual
+
+    history.forEach((operation, index) => {
+        let operationElement = document.createElement('div');
+        operationElement.classList.add('history-item');
+        operationElement.textContent = operation;
+        historyDiv.appendChild(operationElement);
+    });
 }
